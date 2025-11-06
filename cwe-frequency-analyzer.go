cf86ca4ae0a30dd2dcaa8acfb73d6293485c4dcd
dogasantos <douglas.santos@csurface.net>
2025-11-06 10:59:08 -0300

    /home/ubuntu/

--- cwe-frequency-analyzer.go
+++ cwe-frequency-analyzer.go
@@ -47,7 +47,7 @@ func main() {
 	fmt.Println("Generates statistical CWE frequency map from training data")
 	fmt.Println("=================================================================\n")
 
-	resourcesPath := "/home/ubuntu/cwecapec/resources"
+	resourcesPath := "cwecapec/resources"
 	inputPath := resourcesPath + "/training_data_reclassified.json"
 	outputPath := resourcesPath + "/cwe_frequency_map.json"
 
